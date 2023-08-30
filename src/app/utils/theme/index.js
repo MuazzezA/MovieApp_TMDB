ed7e2fb553346a83f61ae468efddf1356545087e
MuazzezA <ayd.muazzez@gmail.com>
2023-08-30 15:22:52 +0300

    feat: Create theme color and size

---
+++ src/app/utils/theme/index.js
@@ -0,0 +1,24 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  gray: '#EFEFF0',
+  success: '#4caf50',
+  error: '#f44336',
+  warning: '#ffeb3b',
+};
+
+const sizes = {
+  h1: 22,
+  h7: 14,
+  h8: 12,
+  base: 16,
+  radius: 12,
+  padding: 24,
+  width,
+  height,
+};
+
+export {colors, sizes};

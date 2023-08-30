9fa3d4f1177c53b09b4ae06aa121632b2d039e32
MuazzezA <ayd.muazzez@gmail.com>
2023-08-30 13:14:09 +0300

    feat: Create home screen

---
+++ src/app/screens/home-screen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};

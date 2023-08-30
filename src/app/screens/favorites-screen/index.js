2b233b208c8087c6d7c43cbbd50c2e257cc1a6a8
MuazzezA <ayd.muazzez@gmail.com>
2023-08-30 13:15:00 +0300

    feat: Create favorites screen

---
+++ src/app/screens/favorites-screen/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+export const FavoritesScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>Favorites Screen</Text>
+    </View>
+  );
+};
--- src/app/screens/index.js
+++ src/app/screens/index.js
@@ -1,2 +1,3 @@
 export * from './home-screen';
 export * from './movie-detail-screen';
+export * from './favorites-screen';

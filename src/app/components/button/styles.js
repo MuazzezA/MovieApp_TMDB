4f4f47a10e45c9499887043b9b82419959133ec3
MuazzezA <ayd.muazzez@gmail.com>
2023-08-30 18:42:33 +0300

    style: button text font size

--- src/app/components/button/styles.js
+++ src/app/components/button/styles.js
@@ -3,7 +3,7 @@ import {colors, sizes} from '../../utils/theme';
 
 export default StyleSheet.create({
   button: {
-    width: sizes.width * 0.45,
+    width: sizes.width * 0.42,
     backgroundColor: colors.gray,
     height: sizes.componentHeight,
     borderRadius: sizes.radius,
@@ -19,11 +19,12 @@ export default StyleSheet.create({
   },
   iconContainer: {
     position: 'absolute',
-    left: sizes.padding,
+    left: sizes.padding * 0.7,
   },
   titleText: {
     fontFamily: 'Inter-Medium',
     position: 'absolute',
+    fontSize: sizes.h8,
     paddingLeft: sizes.padding,
   },
 });

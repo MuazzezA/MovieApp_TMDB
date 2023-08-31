7d714c9ddb1f3fb8d63649230705f2225adc6143
MuazzezA <ayd.muazzez@gmail.com>
2023-08-31 22:39:06 +0300

    feat: Create home screen

--- src/app/screens/home-screen/index.js
+++ src/app/screens/home-screen/index.js
@@ -1,11 +1,77 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {ActivityIndicator, FlatList, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import styles from './styles';
+import {Text, Button, MovieCard, TextInput} from '../../components';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [searchText, setSearchText] = useState('');
+  const [results, setResults] = useState([]);
   return (
     <View style={styles.root}>
-      <Text>Home Screen</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Movie List</Text>
+        <Button
+          icon={'Heart'}
+          title="Favorite Movies"
+          onPress={() => {
+            navigation.navigate('favorites-screen');
+          }}
+        />
+      </View>
+
+      <TextInput
+        value={searchText}
+        onChangeText={text => {
+          setSearchText(text);
+          console.log('text : ', text);
+        }}
+        onBlur={() => {
+          console.log('onBlur');
+        }}
+      />
+
+      <View style={styles.shortButtonsContainer}>
+        <Button
+          icon={'Menu'}
+          title="Short By Category"
+          onPress={() => {
+            console.log('Go to Details');
+          }}
+        />
+        <Button
+          icon={'Stars'}
+          title="Short By Rating"
+          onPress={() => {
+            console.log('Go to Details');
+          }}
+        />
+      </View>
+
+      {results ? (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={results}
+          ItemSeparatorComponent={() => {
+            return <View style={styles.seperator} />;
+          }}
+          renderItem={({item}) => {
+            return (
+              <MovieCard
+                data={item}
+                onPress={() => {
+                  navigation.navigate('movie-detail-screen', {
+                    movieId: item.id,
+                  });
+                }}
+              />
+            );
+          }}
+        />
+      ) : (
+        <ActivityIndicator style={styles.activityIndicator} />
+      )}
     </View>
   );
 };
--- src/app/screens/home-screen/styles.js
+++ src/app/screens/home-screen/styles.js
@@ -1,7 +1,35 @@
 import {StyleSheet} from 'react-native';
+import {colors, sizes} from '../../utils/theme';
 
 export default StyleSheet.create({
   root: {
     flex: 1,
+    backgroundColor: colors.white,
+    paddingTop: sizes.padding,
+    paddingHorizontal: sizes.padding,
+  },
+  seperator: {
+    height: 1,
+    backgroundColor: colors.gray,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  titleText: {
+    fontSize: sizes.h1,
+    fontFamily: 'Inter-Bold',
+    marginVertical: 10,
+  },
+  shortButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: sizes.padding,
+  },
+  activityIndicator: {
+    flex: 0.8,
+    justifyContent: 'center',
   },
 });

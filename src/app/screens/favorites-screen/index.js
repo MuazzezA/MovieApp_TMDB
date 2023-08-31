7c87d3204b11769a0dea07d9ec304babe94747fc
MuazzezA <ayd.muazzez@gmail.com>
2023-08-31 22:51:39 +0300

    feat: Create favorite screen

--- src/app/screens/favorites-screen/index.js
+++ src/app/screens/favorites-screen/index.js
@@ -1,11 +1,80 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {FlatList, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import styles from './styles';
+import {Text, Button, MovieCard, TextInput, Header} from '../../components';
 
 export const FavoritesScreen = () => {
+  const navigation = useNavigation();
+  const [searchText, setSearchText] = React.useState('');
+  const [results, setResults] = React.useState();
+
   return (
     <View style={styles.root}>
-      <Text>Favorites Screen</Text>
+      <Header
+        leftIcon={'ChevronLeft'}
+        onPressLeftIcon={() => navigation.goBack()}
+      />
+      <Text style={styles.titleText}>Favorite Movies</Text>
+
+      <TextInput
+        value={searchText}
+        onChangeText={text => {
+          setSearchText(text);
+          console.log('text : ', text);
+        }}
+        onBlur={() => {
+          console.log('onBlur');
+        }}
+      />
+
+      <View style={styles.shortButtonsContainer}>
+        <Button
+          icon={'Menu'}
+          title="Short By Category"
+          onPress={() => {
+            console.log('short by category');
+          }}
+        />
+        <Button
+          icon={'Stars'}
+          title="Short By Rating"
+          onPress={() => {
+            console.log('short by rating');
+          }}
+        />
+      </View>
+
+      {results ? (
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={results}
+          ItemSeparatorComponent={() => {
+            return <View style={styles.seperator} />;
+          }}
+          renderItem={({item}) => {
+            return (
+              <MovieCard
+                data={item}
+                onPress={() => {
+                  navigation.navigate('movie-detail-screen', {
+                    movieId: item.id,
+                  });
+                }}
+              />
+            );
+          }}
+        />
+      ) : (
+        <View style={styles.notFoundDataTextContainer}>
+          <Text style={styles.notdataTitle}>
+            There are no movies in favorites yet.
+          </Text>
+          <Text style={styles.notdataDesc}>
+            To favorite a movie, go to my movie detail and tap the heart.
+          </Text>
+        </View>
+      )}
     </View>
   );
 };

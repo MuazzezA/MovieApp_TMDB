a597b7de83c0f96ed44332699e018ac5e7dbc72c
MuazzezA <ayd.muazzez@gmail.com>
2023-09-01 23:46:16 +0300

    feat: Create redux store

---
+++ src/app/redux/store.js
@@ -0,0 +1,16 @@
+import {configureStore} from '@reduxjs/toolkit';
+import favoriteMoviesReducer from './slices/favorite-movie-slice.js';
+import addFavoriteMovieReducer from './slices/add-favorite-movie-slice.js';
+import movieDetailReducer from './slices/movie-detail-slice.js';
+import movieCreditsReducer from './slices/movie-credits-slice.js';
+import discoverMovieReducer from './slices/discover-movie-slice.js';
+
+export const store = configureStore({
+  reducer: {
+    favoriteMovies: favoriteMoviesReducer,
+    addFavoriteMovie: addFavoriteMovieReducer,
+    movieDetail: movieDetailReducer,
+    movieCredits: movieCreditsReducer,
+    discoverMovie: discoverMovieReducer,
+  },
+});

e051d4e5f3e427b245113399a2fcbdfff9d62344
MuazzezA <ayd.muazzez@gmail.com>
2023-09-02 01:01:49 +0300

    fix: Edit short by category

--- src/app/screens/home-screen/index.js
+++ src/app/screens/home-screen/index.js
@@ -2,8 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {ActivityIndicator, FlatList, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
-import {Text, Button, MovieCard, TextInput} from '../../components';
-import {discoverMovies, getFavoriteMovies} from '../../api';
+import {Text, Button, MovieCard, TextInput, DropDown} from '../../components';
+import {discoverMovies, getFavoriteMovies, getGenres} from '../../api';
 import styles from './styles';
 
 export const HomeScreen = () => {
@@ -12,25 +12,44 @@ export const HomeScreen = () => {
   const [searchText, setSearchText] = useState('');
 
   const [filteredMovie, setFilteredMovie] = useState([{}]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+
   const {data, loading} = useSelector(state => state.discoverMovie);
+  const {data: genres} = useSelector(state => state.genres);
 
   useEffect(() => {
     dispatch(discoverMovies());
     dispatch(getFavoriteMovies());
+    dispatch(getGenres());
   }, []);
 
   useEffect(() => {
     setFilteredMovie(data?.results);
   }, [data]);
 
+  useEffect(() => {
+    if (selectedCategory.length === 0) {
+      setFilteredMovie(data?.results);
+      return;
+    } else {
+      const selectedCategoryIds = selectedCategory.map(category => category.id);
+      const filteredData = data?.results.filter(item => {
+        return item.genre_ids.some(genreId =>
+          selectedCategoryIds.includes(genreId),
+        );
+      });
+      setFilteredMovie(filteredData);
+    }
+  }, [selectedCategory]);
+
   const onChangeSearchText = text => {
     setSearchText(text);
-
     const filteredData = data?.results.filter(item => {
       return item.title.toLowerCase().includes(text.toLowerCase());
     });
     setFilteredMovie(filteredData);
   };
+
   if (loading || !filteredMovie) {
     return (
       <ActivityIndicator
@@ -40,8 +59,6 @@ export const HomeScreen = () => {
       />
     );
   }
-
-  console.log('filteredMovie', filteredMovie);
 
   return (
     <View style={styles.root}>
@@ -65,18 +82,18 @@ export const HomeScreen = () => {
       />
 
       <View style={styles.shortButtonsContainer}>
-        <Button
+        <DropDown
+          data={genres.genres}
           icon={'Menu'}
-          title="Short By Category"
-          onPress={() => {
-            console.log('Go to Details');
-          }}
+          title={'Short By Category'}
+          selectedValue={selectedCategory}
+          setSelectedValue={setSelectedCategory}
         />
         <Button
           icon={'Stars'}
           title="Short By Rating"
           onPress={() => {
-            console.log('Go to Details');
+            console.log('Short By Rating');
           }}
         />
       </View>
